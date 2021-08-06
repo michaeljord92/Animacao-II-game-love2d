884a48a48314aebee9b19c7ba46e9f50f5da3379
michaeljord92 <michaeljord92@hotmail.com>
2021-08-06 18:38:49 -0300

    Aula 5.8 Recortes em um sprite sheet final

--- main.lua
+++ main.lua
@@ -27,24 +27,23 @@ function love.load()
     sprite_heigth = img_heigth / 10
 
     sprites = {}
+    sprites.hadouken = {}
     sprites.idle = {}
 
-    for i=0,3 do
-        sprite = love.graphics.newQuad(
-            i * sprite_width, 1 * sprite_heigth, 
-            sprite_width, sprite_heigth, 
-            img_width, img_heigth)
+    sprites.hadouken = get_sprites_by_row(1,4)
+    sprites.idle = get_sprites_by_row(2,4)
 
-        table.insert(sprites.idle, sprite)
-    end
-    
     curFrame = 1
+    state = 'idle'
 end
 
 function love.update(dt)
     curFrame = curFrame + 8 * dt
     if curFrame > 4 then
         curFrame = 1
+        if state == 'hadouken' then
+            state = 'idle'
+        end
     end
 
     fps = love.timer.getFPS()
@@ -52,6 +51,30 @@ end
 
 function love.draw()
     love.graphics.setDefaultFilter('nearest', 'nearest')
-    love.graphics.draw(image, sprites.idle[math.floor(curFrame)], 30, 30, 0 , 2, 2)
+    if state == 'idle' then
+        love.graphics.draw(image, sprites.idle[math.floor(curFrame)], 30, 30, 0 , 2, 2)    
+    elseif state == 'hadouken' then
+        love.graphics.draw(image, sprites.hadouken[math.floor(curFrame)], 30, 30, 0 , 2, 2)    
+    end
     -- love.graphics.print('FPS: ' .. fps, 10, 12)
+end
+
+function love.keypressed(key)
+    if key == 'h' then
+        state = 'hadouken'
+        curFrame = 1
+    end
+end
+
+function get_sprites_by_row(row,numSprites) 
+    local sprites = {}
+    for i=0,numSprites-1 do
+        sprite = love.graphics.newQuad(
+            i * sprite_width, (row -1) * sprite_heigth, 
+            sprite_width, sprite_heigth, 
+            img_width, img_heigth)
+
+        table.insert(sprites, sprite)
+    end
+    return sprites
 end
